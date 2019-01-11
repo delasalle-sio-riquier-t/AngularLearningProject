2fd7556689f9d417db6422821b85532a635b1d52
Riquier <riquier.thomas35@gmail.com>
2019-01-11 16:13:39 +0100

    First commit
    Push the project.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {reject} from 'q';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularLearningProject';
+  isAuth = false;
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
+
+  appareils = [
+
+    {
+      name: 'Machine à laver',
+      status: 'éteint',
+      value: 1
+    },
+    {
+      name: 'Frigo',
+      status: 'allumé',
+      value: 2
+    },
+    {
+      name: 'Ordinateur',
+      status: 'éteint',
+      value: 3
+    }
+  ];
+
+  constructor() {
+
+    setTimeout(
+
+      () => {
+
+        this.isAuth = true;
+
+      }, 4000
+
+    );
+
+  }
+
+  onAllumer() {
+
+    console.log('On allume tout !');
+
+  }
 }
---
+++ src/app/learning-component/learning-component.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LearningComponentComponent } from './learning-component.component';
+
+describe('LearningComponentComponent', () => {
+  let component: LearningComponentComponent;
+  let fixture: ComponentFixture<LearningComponentComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LearningComponentComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LearningComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
